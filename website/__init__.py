e3e5978329b09c016ae54d5474b7b77dabdba5f4
Cameron Henning <cwh35@pitt.edu>
2024-02-29 14:48:01 -0500

    created a route

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'this key is not so secret'
 
+    from .views import views
+    from .auth import auth
+
+    # only a slash '/' means no prefix to go to the route
+    # registering blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
--- website/views.py
+++ website/views.py
@@ -1,1 +1,10 @@
 # Used to store the URL endpoints for the frontend part of the website
+from flask import Blueprint
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+# route for the home page
+def home():
+    return "<h1>Test</h1>"
+
