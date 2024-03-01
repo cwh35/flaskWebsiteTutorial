0eb44fbf07bd0b23e0b220c4835d082339a0d514
Cameron Henning <cwh35@pitt.edu>
2024-03-01 14:20:39 -0500

    added format and buttons to sign up sheet

--- website/auth.py
+++ website/auth.py
@@ -5,6 +5,8 @@ auth = Blueprint('auth', __name__)
 # defining login, logout, and sign up routes & URLs
 @auth.route('/login')
 def login():
+    # you can pass variables to your templates that show up on the website
+    # example, text="testing" will show up on the login page
     return render_template("login.html")
 
 @auth.route('/logout')
