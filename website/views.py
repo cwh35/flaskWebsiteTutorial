8e4987ba4392d360b080bcb67ac8b74e1e99beb4
Cameron Henning <cwh35@pitt.edu>
2024-03-04 16:32:40 -0500

    Added flask login module

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 # Used to store the URL endpoints for the frontend part of the website
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 # route for the home page
 def home():
     return render_template("home.html")
