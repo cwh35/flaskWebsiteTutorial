9dec4ded3e0f5fdd2ef60151670b6eccbb8c2195
Cameron Henning <cwh35@pitt.edu>
2024-03-04 15:12:30 -0500

    Added feature for logging in

--- website/auth.py
+++ website/auth.py
@@ -9,6 +9,19 @@ auth = Blueprint('auth', __name__)
 # defining login, logout, and sign up routes & URLs
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # looking for a specific user/entry, return the first result
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully.', category='success')
+            else:
+                flash('Password is not correct, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     # you can pass variables to your templates that show up on the website
     # example, text="testing" will show up on the login page
     return render_template("login.html")
@@ -25,8 +38,12 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists.", category='error')
+
         # message flashing: alert the user that they did something wrong
-        if len(email) < 4:
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters.", category='error')
         elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category='error')
@@ -36,7 +53,7 @@ def sign_up():
             flash("Password must be at least 7 characters.", category='error')
         else:
             # add user to database
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash("Account has been created!", category='success')
